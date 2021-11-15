a5e076ba4c0b19205705c3c52ea1e97bb3a95b93
kvshravan <kandadi.shravan@iiitg.ac.in>
2021-11-15 15:20:41 +0530

    Added tests

--- submissiontool/faculty/tests.py
+++ submissiontool/faculty/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class URLTests(TestCase):
+    def test_faculty_home(self):
+        response = self.client.get('/faculty',{},True)
+        self.assertEqual(response.status_code, 200)
--- submissiontool/student/tests.py
+++ submissiontool/student/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class URLTests(TestCase):
+    def test_student_home(self):
+        response = self.client.get('/student',{},True)
+        self.assertEqual(response.status_code, 200)
